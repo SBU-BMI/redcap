ec4385ae17cdc898406e414a736eb1d63480826f
Jonas Almeida <jalmeida@mathbiol.org>
2017-01-06 18:19:13 -0500

    HELLO

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ console.log('index.js started at '+Date());
 var http = require('http');
 const PORT= process.env.PORT || 8080;
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    response.end('Hello '+process.env.HELLO+'!! Path Hit: ' + request.url);
 }
 function handleRequest(request, response){
     response.end('at '+Date()+' Path Hit: ' + request.url);
