9acb1b6c3fdbcc0d5d22661722f0f846f952a881
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-07 14:53:39 -0500

    hello again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ console.log('index.js started at '+Date());
 var http = require('http');
 const PORT= process.env.PORT || 8080;
 function handleRequest(request, response){
-    response.end('Hello '+process.env.hello+', at '+Date()+' Path Hit: ' + request.url);
+    response.end('Hello at '+Date()+' Path Hit: ' + request.url);
 }
 var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
